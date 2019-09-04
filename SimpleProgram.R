5c9a02832867fa8f57b435d52ff42d8b8f2d4603
Jianing Dong <djn1996@tamu.edu>
2019-09-04 09:46:43 -0500

    add program generating data and calculating beta and MSE

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -8,8 +8,13 @@ sigma = 2 # noise standard deviation
 beta = rep(2,p) # true vector of coefficients
 X = matrix(rnorm(n*p), n, p) # n by p matrix of predictors
 
+# using functions from other r script
+source("FunctionsLM.R")
+
 # [ToDo] Use generateY function to generate Y
-
+Y <- generateY(X, beta, sigma, seed = 5832652)
 # [ToDo] Use calculateBeta function to calculate beta_LS
-
+beta_LS <-  calculateBeta(X, Y)
 # [ToDo] Use calculateMSE to assess the estimation error measured by squared eucledian distance - ||beta - beta_LS||_2^2
+MSE <- calculateMSE(beta, beta_LS)
+
